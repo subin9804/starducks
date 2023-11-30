d0b7e7b42d572374d8213b6b5aa5d1e134f3dc3a
zy_zero <jiyoungjang0307@gmail.com>
2023-11-30 17:27:56 +0900

    ìì코ë이전 코드 ë³복구

--- src/main/java/org/kosta/starducks/mypage/controller/ScheduleController.java
+++ src/main/java/org/kosta/starducks/mypage/controller/ScheduleController.java
@@ -1,19 +1,14 @@
 package org.kosta.starducks.mypage.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/schedules")
-@RequiredArgsConstructor
 public class ScheduleController {
 
-    @GetMapping("/show")
-    public String main() {
+    @GetMapping("/sche")
+    public String showSchedule() {
         return "mypage/schedule/Schedule";
     }
-
 
 }
