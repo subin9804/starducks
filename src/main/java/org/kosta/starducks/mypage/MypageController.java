4ca5e0818dcfc08d8b8acd3032b3b6a5395152d5
kiimnj <1najin@naver.com>
2023-11-30 11:35:52 +0900

    feat: 컨트롤러 연결 테스트

--- src/main/java/org/kosta/starducks/mypage/MypageController.java
+++ src/main/java/org/kosta/starducks/mypage/MypageController.java
@@ -1,5 +1,6 @@
 package org.kosta.starducks.mypage;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MypageController {
     @GetMapping("/mypage")
-    public String mypage() {
-        return "mypage/main";
+    public String mypage(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "mypage/index";
     }
 }
