2f1c8515fe30d29caccdcb8721935f6b19fde5b6
kiimnj <1najin@naver.com>
2023-11-30 16:04:01 +0900

    feat: 샘플 삭제, attendance.html로 통일

--- src/main/java/org/kosta/starducks/mypage/AttendanceController.java
+++ src/main/java/org/kosta/starducks/mypage/AttendanceController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AttendanceController {
-    @GetMapping("/mypage")
-    public String Mypage(Model model) {
+    @GetMapping("/attendance")
+    public String Attendance(Model model) {
         model.addAttribute("data", "hello!!");
-        return "mypage/index";
+        return "mypage/attendance/attendance";
     }
 }
