64c874da45a4e18acc90856b979d8f4bd203bd93
kiimnj <1najin@naver.com>
2023-11-30 12:09:16 +0900

    fix: 폴더,파일명 정리 mypage, calender -> attendance

--- src/main/java/org/kosta/starducks/mypage/AttendanceController.java
+++ src/main/java/org/kosta/starducks/mypage/AttendanceController.java
@@ -1,14 +1,13 @@
 package org.kosta.starducks.mypage;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MypageController {
+public class AttendanceController {
     @GetMapping("/mypage")
-    public String mypage(Model model) {
+    public String Mypage(Model model) {
         model.addAttribute("data", "hello!!");
         return "mypage/index";
     }
