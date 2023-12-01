5ef1c2bc99cc0490fdd82313713a47ce3a331967
kiimnj <1najin@naver.com>
2023-12-01 17:20:05 +0900

    feat: 출퇴근하기 데이터 인풋

---
+++ src/main/java/org/kosta/starducks/mypage/repository/AttendanceRepository.java
@@ -0,0 +1,17 @@
+package org.kosta.starducks.mypage.repository;
+
+import org.kosta.starducks.mypage.entity.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AttendanceRepository  extends JpaRepository<Attendance, Long> {
+
+    @Query("SELECT a.workDate, a.startTime, a.endTime FROM Attendance a WHERE a.emp.id = :empId")
+    List<Object[]> findDailyAttendanceByEmpId(@Param("empId") Long empId);
+    Optional<Attendance> findByWorkIdOrderByWorkDate(Long workId);
+
+}
