c52168cbe784e3e35fd96c02ecf576b5dbf05a5f
kiimnj <1najin@naver.com>
2023-12-05 15:35:29 +0900

    fix: AttendanceDto 컨플릭트 해결

--- src/main/java/org/kosta/starducks/mypage/dto/AttendanceDto.java
+++ src/main/java/org/kosta/starducks/mypage/dto/AttendanceDto.java
@@ -1,9 +1,5 @@
 package org.kosta.starducks.mypage.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import org.kosta.starducks.hr.entity.Employee;
 
 import java.time.LocalDate;
