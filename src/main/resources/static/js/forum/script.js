a9dbf37c6afea1966f1254deab6e2e3de8856285
lhg0529 <lhg0529@naver.com>
2023-12-06 14:08:49 +0900

    내 스크립트 되살리기

--- src/main/resources/static/js/forum/script.js
+++ src/main/resources/static/js/forum/script.js
@@ -1,55 +1,64 @@
-var productIndex = 0;
+function quilljsediterInit() {
+    var options = {
+        modules: {
+            toolbar: [
+                [{ header: [1, 2, false] }],
+                ['bold', 'italic', 'underline'],
+                ['image', 'code-block'],
+                [{ list: 'ordered' }, { list: 'bullet' }]
+            ]
+        },
+        placeholder: '자세한 내용을 입력해 주세요!',
+        theme: 'snow'
+    };
 
-/* 상품 추가 버튼 클릭 시 동적으로 상품 입력 필드 추가 */
-function addProductField() {
-    var productFields = document.getElementById('productFields');
+    var quill = new Quill('#editor', options);
 
-    var productField = document.createElement('div');
-    productField.appendChild(document.createElement('label'));
-    productField.lastChild.htmlFor = 'product';
-    productField.lastChild.textContent = '입고상품 : ';
+    // 에디터 내용이 변경될 때마다 숨겨진 입력 필드에 값을 업데이트
+    quill.on('text-change', function() {
+        document.getElementById("quill_html").value = quill.root.innerHTML;
+    });
 
-    var selectElement = document.createElement('select');
-    selectElement.id = 'product';
-    selectElement.name = 'productCodes[' + productIndex + ']';
+    // 이미지 업로드 핸들러
+    quill.getModule('toolbar').addHandler('image', function() {
+        selectLocalImage();
+    });
 
-    var defaultOption = document.createElement('option');
-    defaultOption.value = '';
-    defaultOption.text = '상품 선택하세요';
-    selectElement.appendChild(defaultOption);
+    // 이미지 업로드 함수
+    function selectLocalImage() {
+        const fileInput = document.createElement('input');
+        fileInput.setAttribute('type', 'file');
+        fileInput.click();
 
-    /* JavaScript로 서버에서 전달한 데이터 사용 */
-    /* products는 서버에서 전달한 상품 목록 데이터로 가정 */
-    var products = JSON.parse(document.getElementById('productFields').getAttribute('data-products'));
+        fileInput.addEventListener("change", function() {
+            const formData = new FormData();
+            const file = fileInput.files[0];
+            formData.append('uploadFile', file);
 
-    for (var i = 0; i < products.length; i++) {
-        var optionElement = document.createElement('option');
-        optionElement.value = products[i].productCode;
-        optionElement.text = products[i].productName;
-        selectElement.appendChild(optionElement);
+            // 이미지 파일을 서버에 업로드하는 AJAX 요청
+            $.ajax({
+                type: 'post',
+                enctype: 'multipart/form-data',
+                url: '/board/register/imageUpload',
+                data: formData,
+                processData: false, // processData와 contentType을 false로 설정
+                contentType: false, // 파일 업로드 시 이 옵션이 필요
+                dataType: 'json',
+                success: function (data) {
+                    // 업로드된 이미지를 에디터에 삽입
+                    const range = quill.getSelection();
+                    data.uploadPath = data.uploadPath.replace(/\\/g, '/');
+                    quill.insertEmbed(range.index, 'image', "/board/display?fileName=" + data.uploadPath + "/" + data.uuid + "_" + data.fileName);
+                },
+                error: function (err) {
+                    console.error("이미지 업로드 실패:", err);
+                }
+            });
+        });
     }
 
-    productField.appendChild(selectElement);
-
-    productField.appendChild(document.createElement('label'));
-    productField.lastChild.htmlFor = 'quantity';
-    productField.lastChild.textContent = ' 입고수량';
-
-    var inputElement = document.createElement('input');
-    inputElement.type = 'number';
-    inputElement.id = 'quantity';
-    inputElement.name = 'inboundQuantities[' + productIndex + ']';
-    inputElement.required = true;
-
-    productField.appendChild(inputElement);
-
-    productField.appendChild(document.createElement('br'));
-
-    productFields.appendChild(productField);
-    productIndex++;
 }
-<<<<<<< HEAD
-=======
+
 
 // 페이지 로드 완료 시 에디터 초기화
 window.onload = function() {
@@ -150,4 +159,4 @@ $(document).ready(function() {
     $("#content").html(content);
 });
 */
->>>>>>> master
+
