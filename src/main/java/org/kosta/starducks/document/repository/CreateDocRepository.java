116a16eefae3597547052ed192f3361826459bcc
kiimnj <1najin@naver.com>
2023-12-08 09:58:27 +0900

    feat: 업무기안서 컨트롤러, 사이드바 연결

--- src/main/java/org/kosta/starducks/document/repository/CreateDocRepository.java
+++ src/main/java/org/kosta/starducks/document/repository/CreateDocRepository.java
@@ -1,8 +1,8 @@
-//package org.kosta.starducks.document.repository;
-//
-//import org.kosta.starducks.document.entity.Document;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface CreateDocRepository extends JpaRepository<Document, Long>  {
-//
-//}
+package org.kosta.starducks.document.repository;
+
+import org.kosta.starducks.document.entity.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreateDocRepository extends JpaRepository<Document, Long>  {
+
+}
