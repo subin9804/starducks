d24cfbe40daba579bacdb0b4d6d05b58382c1c5d
lhg0529 <lhg0529@naver.com>
2023-12-10 00:25:24 +0900

    비밀 번호 찾기 페이지 로그인 페이지와 동일하게 구성 완료. 유효성 검사만 넣으면 완료

--- src/main/resources/static/js/auth/script.js
+++ src/main/resources/static/js/auth/script.js
@@ -1,9 +1,15 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     // 서버에서 전달된 메시지 확인
-//     var message = /*[[${message}]]*/ 'null';
-//
-//     // 메시지가 존재하면 알림창으로 표시
-//     if (message !== 'null' && message !== '') {
-//         alert(message);
-//     }
-// });
+// 이메일 인풋은 valid가 되려면 @가 있는 경우메만 그렇다보니
+// 다른 인풋들과 동일하게 아무 값이나 입력됐어도 플로팅 라벨이 유지되려면 이렇게 설정해줘야 한다
+document.addEventListener("DOMContentLoaded", function() {
+    var inputs = document.querySelectorAll(".form-floating input");
+
+    inputs.forEach(function(input) {
+        input.addEventListener("blur", function() {
+            if (this.value !== "") {
+                this.classList.add("has-value");
+            } else {
+                this.classList.remove("has-value");
+            }
+        });
+    });
+});
