71e75c70f361e6d0e7973564d275aa02466b6f0c
lhg0529 <lhg0529@naver.com>
2023-12-10 00:57:59 +0900

    비밀번호 찾기 사번, 이메일 주소 유효성 검사 완료. 이메일 전송 알림창만 자고 일어나서...

--- src/main/java/org/kosta/starducks/auth/handler/CustomFailHandler.java
+++ src/main/java/org/kosta/starducks/auth/handler/CustomFailHandler.java
@@ -12,6 +12,12 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
+
+/**
+ * 시큐리티 컨피그에서 사용하는 용도. 로그인 실패 때 에러메시지 출력을 위한 핸들러
+ * 시큐리티 사용 때문에 세션을 통해서 확인해야함
+ */
+
 @Component
 public class CustomFailHandler extends SimpleUrlAuthenticationFailureHandler {
 
@@ -25,8 +31,8 @@ public class CustomFailHandler extends SimpleUrlAuthenticationFailureHandler {
     } else {
       errorMessage = "알 수 없는 이유로 로그인에 실패하였습니다. 관리자에게 문의하세요.";
     }
-    errorMessage = URLEncoder.encode(errorMessage, StandardCharsets.UTF_8);
-    setDefaultFailureUrl("/login?error=true&exception=" + errorMessage);
+    errorMessage = URLEncoder.encode(errorMessage, StandardCharsets.UTF_8); //인코딩을 해줘야 주소창에서 한글을 인식한다
+    setDefaultFailureUrl("/login?error=true&exception=" + errorMessage); //주소창에 에러 메시지가 추가됨
 
     super.onAuthenticationFailure(request, response, exception);
   }
--- src/main/java/org/kosta/starducks/configs/SecurityConfig.java
+++ src/main/java/org/kosta/starducks/configs/SecurityConfig.java
@@ -35,21 +35,20 @@ public class SecurityConfig {
     @Autowired
     private CustomFailHandler customFailHandler;
 
-    @Bean //로그인 유지 기능 rememberMe 나중에 추가하기
+    @Bean //시큐리티 옵션을 커스텀할 수 있는 곳
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-            .csrf(AbstractHttpConfigurer::disable) //나중에 지워주면 좋음  Cross-Site Request Forgery (CSRF) 보호 기능을 비활성화
+            .csrf(AbstractHttpConfigurer::disable) // ajax 사용하려면 토큰 필요. 우선은 disable 해둠.  Cross-Site Request Forgery (CSRF) 보호 기능을 비활성화
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/**","/login", "/forgot-password")
+                .requestMatchers("/**","/login", "/forgot-password") //로그인 안해도 접속 가능한 주소 /**는 모든 하위 주소에 접근 가능하게 만듦
                 .permitAll()
                 .anyRequest().authenticated())
             .formLogin(form -> form
-                .loginPage("/login")
-//                .loginProcessingUrl("/loginProc")
+                .loginPage("/login") //로그인 하는 페이지
                 .usernameParameter("empId") //사원 번호를 통해서 정보 조회
                 .defaultSuccessUrl("/", true) //로그인 성공 시 메인 페이지로
-                .failureHandler(customFailHandler) //페이지 자체에 로그인 실패 메시지 표시를 위해 커스텀
+                .failureHandler(customFailHandler) //페이지 자체에 로그인 실패 메시지 표시를 위해 커스텀핸들러 따로 제작함
                 .permitAll())
             .logout(LogoutConfigurer::permitAll);
 
@@ -73,12 +72,4 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-
-
-    /**
-     * 로그인에서 사번이나 비밀번호가 일치하지 않으면 페이지에 에러 메시지를 띄우기 위한 핸들러
-     * @return
-     */
-
-
 }
