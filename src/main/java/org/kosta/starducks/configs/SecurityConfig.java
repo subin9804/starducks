3a2656c6a59b46f92f1828e6357b208cb35535fd
lhg0529 <lhg0529@naver.com>
2023-12-14 11:12:19 +0900

    비밀번호 찾기 페이지에서도 폼에 토큰이 활성화되게 수정. th:action 잊지 말자

--- src/main/java/org/kosta/starducks/configs/SecurityConfig.java
+++ src/main/java/org/kosta/starducks/configs/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
