4c1a8bab215f6925cac7d76e92f9f59833df8649
lhg0529 <lhg0529@naver.com>
2023-12-14 11:16:12 +0900

    zjalt

--- src/main/java/org/kosta/starducks/configs/SecurityConfig.java
+++ src/main/java/org/kosta/starducks/configs/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-//            .csrf(AbstractHttpConfigurer::disable) // ajax 사용하려면 토큰 필요. Cross-Site Request Forgery (CSRF) 보호 기능을 비활성화
+//            .csrf(AbstractHttpConfigurer::disable) // ajax 사용하려면 토큰 필요.
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/login", "/forgotPwd") //로그인 안해도 접속 가능한 주소 /**는 모든 하위 주소에 접근 가능하게 만듦. 나중에 지울 예정
                 .permitAll()
