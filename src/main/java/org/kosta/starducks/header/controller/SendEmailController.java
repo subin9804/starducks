28807460ad6692c0912d16f78b909941f7b86824
PURAVIDAJI <ijk9501@naver.com>
2023-12-15 12:17:32 +0900

    받은 메일함 pagenation 구현 완

--- src/main/java/org/kosta/starducks/header/controller/SendEmailController.java
+++ src/main/java/org/kosta/starducks/header/controller/SendEmailController.java
@@ -6,11 +6,14 @@ import lombok.AllArgsConstructor;
 import org.kosta.starducks.header.dto.EmailDto;
 import org.kosta.starducks.header.dto.RSEmailDto;
 import org.kosta.starducks.header.service.sendEmailService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Comparator;
 import java.util.List;
@@ -48,13 +51,18 @@ public class SendEmailController {
     }
 
     @GetMapping("/sentlist")
-    public String getSentEmailList(Model model) {
+    public String getSentEmailList(Model model,
+                                   @RequestParam(defaultValue = "0") int page,
+                                   @RequestParam(defaultValue = "9") int size) {
         try {
 
-            List<RSEmailDto> emails =emailService.fetchSentEmails();
-            emails.sort(Comparator.comparing(RSEmailDto::getSentDate).reversed());
+            Page<RSEmailDto> emails =emailService.fetchSentEmails(PageRequest.of(page, size));
+
+
 
             model.addAttribute("emails", emails);
+
+
         } catch (Exception e) {
             e.printStackTrace();
             // Handle exceptions appropriately
