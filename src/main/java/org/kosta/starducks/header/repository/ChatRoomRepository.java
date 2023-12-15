46d7ffc53811a107406c91c4a2cab10fc78ff77c
lhg0529 <lhg0529@naver.com>
2023-12-15 17:06:16 +0900

    merge2

--- src/main/java/org/kosta/starducks/header/repository/ChatRoomRepository.java
+++ src/main/java/org/kosta/starducks/header/repository/ChatRoomRepository.java
@@ -16,7 +16,7 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
   List<ChatRoom> findAllByRoomName(String roomName);
 
   // ChatRoom 목록조회 - 조건정렬순, RoomName 검색, 정확히 일치
-  List<ChatRoom> findAllByRoomName(String roomName, TypeCache.Sort sort);
+//  List<ChatRoom> findAllByRoomName(String roomName, TypeCache.Sort sort);
 
   // ChatRoom 목록조회 - 기본정렬순, RoomName 검색, 포함 일치
   List<ChatRoom> findAllByRoomNameContaining(String roomName);
