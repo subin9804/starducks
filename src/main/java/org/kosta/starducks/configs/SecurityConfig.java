e448a99e324a87dcabc0151ac2ff9056de50ee43
zy_zero <jiyoungjang0307@gmail.com>
2023-12-21 14:35:47 +0900

    í.csrf(AbstractHttpConfigurer::disable) 살리고 돌아가는 것 확인

--- src/main/java/org/kosta/starducks/configs/SecurityConfig.java
+++ src/main/java/org/kosta/starducks/configs/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
     @Autowired
     private CustomFailHandler customFailHandler;
@@ -25,7 +26,7 @@ public class SecurityConfig{
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-//                .csrf(AbstractHttpConfigurer::disable) // ajax 사용하려면 토큰 필요. 우선은 disable 해둠.  Cross-Site Request Forgery (CSRF) 보호 기능을 비활성화
+                .csrf(AbstractHttpConfigurer::disable) // ajax 사용하려면 토큰 필요. 우선은 disable 해둠.  Cross-Site Request Forgery (CSRF) 보호 기능을 비활성화
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/forgotPwd") //로그인 안해도 접속 가능한 주소 /**는 모든 하위 주소에 접근 가능하게 만듦. 나중에 지울 예정
                         .permitAll()
