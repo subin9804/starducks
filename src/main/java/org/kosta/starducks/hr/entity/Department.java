183024b72677c56bbf76eea3f77a7c0e31a3f8d5
lhg0529 <lhg0529@naver.com>
2023-12-22 00:36:15 +0900

    채팅 사원 목록 css 대충 마무리. 채팅방 목록 css 시작

--- src/main/java/org/kosta/starducks/hr/entity/Department.java
+++ src/main/java/org/kosta/starducks/hr/entity/Department.java
@@ -14,30 +14,46 @@ public class Department {
 
     @Id
     private int deptId; // 부서코드
-    private String deptName;    // 부서 이름
+    private String deptName;    // 부서 이름 (한글)
+    private String deptNameEng; // 부서 이름 (영어)
     private String deptRepTel;  // 부서 대표전화
 
     @OneToMany(mappedBy="dept")
     private List<Employee> emps;
 
-    public Department(int deptId, String deptName, String deptRepTel) {
+    public Department(int deptId, String deptName, String deptNameEng, String deptRepTel) {
         this.deptId = deptId;
         this.deptName = deptName;
+        this.deptNameEng = deptNameEng;
         this.deptRepTel = deptRepTel;
     }
 
     /** 해당 부서에 재직중인 인원 */
     public int getRunningEmp() {
-        int cnt = 0;
-
-        List<Employee> emps = getEmps();
+        int count = 0;
         for(Employee emp : emps) {
             if(!emp.isStatus()) {
-                cnt++;
+                count++;
             }
         }
-
-        return cnt;
+        return count;
     }
 
+    public String getRoleForDeptName() {
+        switch(this.deptName) {
+            case "재무부":
+                return "ROLE_FINA";
+            case "인사부":
+                return "ROLE_HR";
+            case "물류유통부":
+                return "ROLE_LOGISTIC";
+            case "총무부":
+                return "ROLE_GENERAL";
+            case "사장실":
+                return "ROLE_EXEC";
+            default:
+                return "ROLE_UNKNOWN";
+        }
+    }
 }
+
