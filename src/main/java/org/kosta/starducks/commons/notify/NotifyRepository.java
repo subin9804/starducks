36bcd93519fe7e895fcc50d4ad6e7fc5db7e07f7
subin9804 <subin9804@naver.com>
2023-12-25 18:11:14 +0900

    feat: 알림 기능 구현 중
    entity, repository 구현 완료
    custom annotation인 NeedNotify 구현
    
    --end

---
+++ src/main/java/org/kosta/starducks/commons/notify/NotifyRepository.java
@@ -0,0 +1,7 @@
+package org.kosta.starducks.commons.notify;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotifyRepository extends JpaRepository<Notify, Long> {
+
+}
