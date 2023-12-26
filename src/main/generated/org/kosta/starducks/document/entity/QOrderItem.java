1839e2323a97081d50b7337cb70af7f1d297ac4b
kiimnj <1najin@naver.com>
2023-12-26 09:42:49 +0900

    fix: 근태관리 서버 디버깅문 제거

---
+++ src/main/generated/org/kosta/starducks/document/entity/QOrderItem.java
@@ -0,0 +1,55 @@
+package org.kosta.starducks.document.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QOrderItem is a Querydsl query type for OrderItem
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QOrderItem extends EntityPathBase<OrderItem> {
+
+    private static final long serialVersionUID = -932734041L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QOrderItem orderItem = new QOrderItem("orderItem");
+
+    public final QDocument document;
+
+    public final NumberPath<Long> orderItemId = createNumber("orderItemId", Long.class);
+
+    public final NumberPath<Long> productCode = createNumber("productCode", Long.class);
+
+    public final NumberPath<Integer> quantity = createNumber("quantity", Integer.class);
+
+    public QOrderItem(String variable) {
+        this(OrderItem.class, forVariable(variable), INITS);
+    }
+
+    public QOrderItem(Path<? extends OrderItem> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QOrderItem(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QOrderItem(PathMetadata metadata, PathInits inits) {
+        this(OrderItem.class, metadata, inits);
+    }
+
+    public QOrderItem(Class<? extends OrderItem> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.document = inits.isInitialized("document") ? new QDocument(forProperty("document"), inits.get("document")) : null;
+    }
+
+}
+
