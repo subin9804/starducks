6b734bcb472f85e85de7c02023a4a1f38ca35b93
lhg0529 <lhg0529@naver.com>
2024-01-01 00:32:29 +0900

    채팅 관련 설명 추가, 컨트롤러 중복, 미사용 정리

--- src/main/java/org/kosta/starducks/header/controller/ChatRoomRestController.java
+++ src/main/java/org/kosta/starducks/header/controller/ChatRoomRestController.java
@@ -3,11 +3,12 @@ package org.kosta.starducks.header.controller;
 import org.kosta.starducks.header.dto.ChatRoomRequestDto;
 import org.kosta.starducks.header.dto.ChatRoomResponseDto;
 import org.kosta.starducks.header.service.ChatRoomService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 
@@ -18,20 +19,27 @@ import java.util.List;
 @RequestMapping("/api/chat/rooms")
 public class ChatRoomRestController {
 
-  @Autowired
-  private ChatRoomService chatRoomService;
+  private final ChatRoomService chatRoomService;
 
-  @PostMapping("/create")
-  public Long createRoom(@RequestBody ChatRoomRequestDto requestDto) {
-    return chatRoomService.createChatRoom(requestDto);
+  public ChatRoomRestController(ChatRoomService chatRoomService) {
+    this.chatRoomService = chatRoomService;
   }
 
-  @GetMapping("/{id}")
-  public ChatRoomResponseDto getRoom(@PathVariable("id") Long id) {
-    return chatRoomService.findById(id);
+  // 채팅방 생성 요청 처리
+  // chatEmpList.html의 스크립트에서 채팅방 생성 부분 fetch('/api/chat/rooms/createChatRoom' 에서 사용 중
+  @PostMapping("/createChatRoom")
+  public ResponseEntity<?> createChatRoom(@RequestBody ChatRoomRequestDto requestDto,
+                                          Principal principal) {
+    Long userId = Long.parseLong(principal.getName()); // 로그인한 사용자의 ID를 가져옴
+    requestDto.getEmpIds().add(userId); // 사용자 ID를 사원 ID 리스트에 추가
+
+    Long chatRoomId = chatRoomService.createChatRoom(requestDto);
+
+    return ResponseEntity.ok().body(chatRoomId);
   }
 
   //  채팅방 목록 페이지에서 로그인한 사원과 관련된 것만 불러오기
+  // chatRoomList.html의 스크립트 function loadChatRooms()에서 사용 중
   @GetMapping("/my-rooms")
   public List<ChatRoomResponseDto> getMyChatRooms(@AuthenticationPrincipal UserDetails userDetails) {
     Long empId = Long.parseLong(userDetails.getUsername());
--- src/main/java/org/kosta/starducks/header/controller/ChatViewController.java
+++ src/main/java/org/kosta/starducks/header/controller/ChatViewController.java
@@ -2,16 +2,12 @@ package org.kosta.starducks.header.controller;
 
 import org.kosta.starducks.auth.dto.CustomUserDetails;
 import org.kosta.starducks.header.dto.ChatMessageResponseDto;
-import org.kosta.starducks.header.dto.ChatRoomRequestDto;
 import org.kosta.starducks.header.dto.ChatRoomResponseDto;
-import org.kosta.starducks.header.entity.ChatRoom;
 import org.kosta.starducks.header.service.ChatMessageService;
 import org.kosta.starducks.header.service.ChatRoomService;
 import org.kosta.starducks.hr.entity.Department;
 import org.kosta.starducks.hr.entity.Employee;
 import org.kosta.starducks.hr.service.EmpService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * 채팅과 관련된 html 페이지들과 연결해주는 뷰 컨트롤러
@@ -71,19 +66,7 @@ public class ChatViewController {
     return "header/chatEmpList";
   }
 
-  // 채팅방 생성 요청 처리
-  @PostMapping("/createChatRoom")
-  public ResponseEntity<?> createChatRoom(@RequestBody ChatRoomRequestDto requestDto,
-                                          Principal principal) {
-    Long userId = Long.parseLong(principal.getName()); // 로그인한 사용자의 ID를 가져옴
-    requestDto.getEmpIds().add(userId); // 사용자 ID를 사원 ID 리스트에 추가
-
-    Long chatRoomId = chatRoomService.createChatRoom(requestDto);
-
-    return ResponseEntity.ok().body(chatRoomId);
-  }
-
-  // 채팅방 상세 페이지
+  // 채팅방 상세 페이지. 메시지를 주고 받는 곳
   @GetMapping("/chatRoom/{roomId}")
   public String chatRoomPage(@PathVariable("roomId") Long roomId, Model model, @AuthenticationPrincipal CustomUserDetails currentUser) {
     List<ChatMessageResponseDto> messages = chatMessageService.getMessagesForChatRoom(roomId);
