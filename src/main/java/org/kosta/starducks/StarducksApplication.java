f87b1199af1c689435e53f483ca17084d704b227
lhg0529 <lhg0529@naver.com>
2024-01-05 11:39:52 +0900

    StarducksApplication에 한국 타임존 추가 시도, 게터, 세터 어노테이션 방식으로 변경

--- src/main/java/org/kosta/starducks/StarducksApplication.java
+++ src/main/java/org/kosta/starducks/StarducksApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import java.util.TimeZone;
+
 //@EnableAspectJAutoProxy(proxyTargetClass = true)
 @SpringBootApplication
 public class StarducksApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(StarducksApplication.class, args);
 	}
-
 }
